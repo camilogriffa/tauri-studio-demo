40bdef112fc810deea27adfdb4597993b221af3c
Camilo Griffa <camilogriffa@protonmail.com>
2022-05-30 13:39:30 -0300

    Actualizacion de proyecto

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dev-app.cobromix.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ir a cobromix
         </a>
       </header>
     </div>
